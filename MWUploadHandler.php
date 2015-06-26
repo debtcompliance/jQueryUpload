ae77cfa9041753c84ff42bc563a2543e8989efef
Aran <aran@organicdesign.co.nz>
2015-06-26 20:06:32 -0300

    start migrating jQuery Upload to new JSON loading methods

---
+++ MWUploadHandler.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * A modified version of the Blueimp UploadHandler class
+ */
+class MWUploadHandler extends UploadHandler {
+
+	/**
+	 * The delete URL needs to be adjusted because it doesn't check if the script URL already has a query-string and to add path info
+	 */
+	protected function set_file_delete_url( $file ) {
+		$path = preg_match( '|jquery_upload_files/(.+)/|', $this->options['upload_dir'], $m ) ? "&path=$m[1]" : '';
+		$file->delete_url = $this->options['script_url'] . "$path&file=" . rawurlencode($file->name);
+		$file->delete_type = $this->options['delete_type'];
+		if ($file->delete_type !== 'DELETE') $file->delete_url .= '&_method=DELETE';
+	}
+
+	/**
+	 * We override the thumbnail creation to return a filetype icon when files can't be scaled as an image
+	 */
+	protected function create_scaled_image( $file, $options ) {
+		if( $result = parent::create_scaled_image( $file, $options ) ) return $result;
+		$icon = jQueryUpload::icon( $file );
+		return symlink( $icon , $options['upload_dir'] . jQueryUpload::thumbFilename( $file ) );
+	}
+
+	/**
+	 * Add info on the user who uploaded the file and the date it was uploaded, and create thumb if it doesn't exist
+	 */
+	protected function get_file_object( $file_name ) {
+
+		// Create the thumb if it doesn't exist
+		$thumb = $this->options['upload_dir'] . 'thumb/' . $file_name;
+		if( !file_exists( jQueryUpload::thumbFilename( $thumb ) ) ) {
+			$this->create_scaled_image( $file_name, $this->options['image_versions']['thumbnail'] );
+		}
+
+		// Call the parent method to create the file object
+		$file = parent::get_file_object( $file_name );
+
+		// Add the meta data to the object
+		if( is_object( $file ) ) {
+			$meta = $this->options['upload_dir'] . 'meta/' . $file_name;
+			$file->info = $file->desc = "";
+
+			// If the meta data file exists, extract and render the content
+			if( is_file( $meta ) ) {
+				$data = unserialize( file_get_contents( $meta ) );
+				$file->info = self::renderData( $data );
+				$file->desc = array_key_exists(2, $data) ? $data[2] : '';
+			}
+			
+			// If the file is a symlink to a file uploaded in the wiki, get the metadata from the wiki file instead
+			elseif( is_link( $this->options['upload_dir'] . $file_name ) ) {
+				$title = Title::newFromText( $file_name, NS_FILE );
+				if( is_object( $title ) && $title->exists() ) {
+					list( $uid, $ts, $file->desc ) = self::getUploadedFileInfo( $title );
+					$file->info = self::renderData( array( $uid, wfTimestamp( TS_UNIX, $ts ) ) );
+				}
+			}
+		}
+		return $file;
+	}
+
+	/**
+	 * Render file data
+	 */
+	public static function renderData( $data ) {
+		global $wgContLang, $wgUser;
+		$user = User::newFromID( $data[0] );
+		$name = $user->getRealName();
+		if( empty( $name ) ) $name = $user->getName();
+		$d = $wgContLang->userDate( $data[1], $wgUser );
+		$t = $wgContLang->userTime( $data[1], $wgUser );
+		return wfMsg( 'jqueryupload-uploadinfo', $name, $d, $t );
+	}
+
+	/**
+	 * Get the description, name and upload time of the passed wiki file
+	 */
+	public static function getUploadedFileInfo( $title ) {
+		$article = new Article( $title );
+		$desc = $article->getPage()->getContent()->getNativeData();
+		$dbr = wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow(
+			'revision',
+			array( 'rev_user', 'rev_timestamp' ),
+			array( 'rev_page' => $title->getArticleID() ),
+			__METHOD__,
+			array( 'ORDER BY' => 'rev_timestamp','LIMIT' => 1 )
+		);
+		return array( $row->rev_user, $row->rev_timestamp, $desc );
+	}
+
+	/**
+	 * We should remove the unused directory after deleting a file
+	 */
+	public function delete() {
+		parent::delete();
+		$dir = $this->options['upload_dir'];
+
+		// Check that the upload dir has no files in it
+		$empty = true;
+		foreach( glob( "$dir/*" ) as $item ) if( is_file( $item ) ) $empty = false;
+
+		// There are no uploaded files in this directory, nuke it
+		// - we need to use rm -rf because it still contains sub-dirs and meta data
+		if( $empty ) exec( "rm -rf $dir" );
+	}
+
+	/**
+	 * We add a meta-data file for the upload in the meta dir
+	 */
+	protected function handle_file_upload( $uploaded_file, $name, $size, $type, $error, $index = null ) {
+		$file = parent::handle_file_upload( $uploaded_file, $name, $size, $type, $error, $index );
+		if( is_object( $file ) ) {
+			$file_path = $this->options['upload_dir'] . $file->name;
+			if( is_file( $file_path ) ) {
+				global $wgUser;
+				$desc = jQueryUpload::$desc[$file->name];
+				$meta = $this->options['upload_dir'] . 'meta/' . $file->name;
+				$data = array( $wgUser->getID(), time(), $desc == wfMsg( 'jqueryupload-enterdesc' ) ? '' : $desc );
+				file_put_contents( $meta, serialize( $data ) );
+			}
+		}
+		return $file;
+	}
+}
--- jQueryUpload.php
+++ jQueryUpload.php
@@ -1,49 +1,15 @@
 <?php
-/**
- * jQueryUpload MediaWiki extension - allows files to be uploaded to the wiki or to specific pages using the jQueryFileUpload module
- *
- * jQueryFileUpload module: https://github.com/blueimp/jQuery-File-Upload
- *
- * @package MediaWiki
- * @subpackage Extensions
- * @author Aran Dunkley (http://www.organicdesign.co.nz/nad)
- */
-if( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
-define( 'JQU_VERSION', "1.3.10, 2015-06-18" );
-
-$wgJQUploadIconPrefix = dirname( __FILE__ ) . '/icons/Farm-Fresh_file_extension_';
-$wgJQUploadFileMagic = 'file';
-$wgHooks['LanguageGetMagic'][] = 'jQueryUpload::onLanguageGetMagic';
-$wgJQUploadFileLinkPopup = true;
-
-$wgAjaxExportList[] = 'jQueryUpload::server';
-
-$wgExtensionFunctions[] = 'wfJQueryUploadSetup';
-$wgSpecialPages['jQueryUpload'] = 'jQueryUpload';
-$wgSpecialPageGroups['jQueryUpload'] = 'media';
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => "jQueryUpload",
-	'descriptionmsg' => "jqueryupload-desc",
-	'url'            => "http://www.organicdesign.co.nz/jQueryUpload",
-	'author'         => array( "[http://www.organicdesign.co.nz/nad Aran Dunkley]", "[http://blueimp.net Sebastian Tschan]" ),
-	'version'        => JQU_VERSION
-);
-
-// If the query-string arg mwaction is supplied, rename action and change mwaction to action
-// - this hack was required because the jQueryUpload module uses the name "action" too
-if( array_key_exists( 'mwaction', $_REQUEST ) ) {
-	$wgJQUploadAction = array_key_exists( 'action', $_REQUEST ) ? $_REQUEST['action'] : false;
-	$_REQUEST['action'] = $_GET['action'] = $_POST['action'] = $_REQUEST['mwaction'];
-}
-
-$dir = dirname( __FILE__ );
-$wgExtensionMessagesFiles['jQueryUpload'] = "$dir/jQueryUpload.i18n.php";
-$wgExtensionMessagesFiles['jQueryUploadAlias'] = "$dir/jQueryUpload.alias.php";
-require( "$dir/upload/server/php/upload.class.php" );
-require( "$dir/jQueryUpload_body.php" );
-
-function wfJQueryUploadSetup() {
-	global $wgJQueryUpload;
-	$wgJQueryUpload = new jQueryUpload();
+if ( function_exists( 'wfLoadExtension' ) ) {
+	$ext = preg_replace( '%^.+/(.+?)\.php$%', '$1', __FILE__ );
+	die(
+		"Deprecated PHP entry point used for <i>$ext</i> extension. Please use wfLoadExtension instead, " .
+		'see <a href="https://www.mediawiki.org/wiki/Extension_registration">Extension registration</a> for more details.'
+	);
+	return;
+} else {
+	die(
+		"<b>Fatal error:</b> This version of the <i>$ext</i> extension requires MediaWiki 1.25+, " .
+		'either <a href="https://mediawiki.org/wiki/Download">upgrade your MediaWiki</a> or download the extension code from the ' .
+		'<a href="https://github.com/OrganicDesign/extensions/tree/MediaWiki-1.24/MediaWiki">1.24 branch</a>.'
+	);
 }
