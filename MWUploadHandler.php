0af4443c81175afce5bd37d6156a9eaf5ba5ba42
Igor Absorto <igorabsorto@riseup.net>
2020-03-27 16:12:46 -0300

    passes object to allow subscriber to handle errors on their side

--- MWUploadHandler.php
+++ MWUploadHandler.php
@@ -124,14 +124,13 @@ class MWUploadHandler extends UploadHandler {
 			$file_path = $this->options['upload_dir'] . $file->name;
 			if( is_file( $file_path ) ) {
 				global $wgUser;
-				if( Hooks::run( 'jQueryUploadCheckFile', [$file_path] ) ) {
+				if( Hooks::run( 'jQueryUploadCheckFile', [&$file, $file_path] ) ) {
 					$desc = jQueryUpload::$desc[$file->name];
 					$meta = $this->options['upload_dir'] . 'meta/' . $file->name;
 					$data = array( $wgUser->getID(), time(), $desc == wfMessage( 'jqueryupload-enterdesc' ) ? '' : $desc );
 					file_put_contents( $meta, serialize( $data ) );
 				} else {
 					unlink($file_path);
-					$file->error = wfMessage( 'jqueryupload-virus' )->text();
 				}
 			}
 		}
